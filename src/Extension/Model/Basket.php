5ece839e76277e5663d554030435e39f1b112dff
Tatyana Koleva <tatjana.koleva@dnhsoft.com>
2024-10-04 17:10:53 +0300

    OXDEV-8761 Cleanup and leave empty module skeleton

--- src/Extension/Model/Basket.php
+++ src/Extension/Model/Basket.php
@@ -7,37 +7,9 @@
 
 declare(strict_types=1);
 
-namespace OxidEsales\ModuleTemplate\Extension\Model;
+namespace OxidEsales\FreeShippingCoupons\Extension\Model;
 
-use OxidEsales\ModuleTemplate\Logging\Service\BasketProductLoggerServiceInterface;
-
-/**
- * @mixin \OxidEsales\Eshop\Application\Model\Basket
- */
 class Basket extends Basket_parent
 {
-    /**
-     * Method overrides eShop method and adds logging functionality.
-     * {@inheritDoc}
-     * @SuppressWarnings(PHPMD.BooleanArgumentFlag)
-     */
-    public function addToBasket(
-        $productID,
-        $amount,
-        $sel = null,
-        $persParam = null,
-        $shouldOverride = false,
-        $isBundle = false,
-        $oldBasketItemId = null
-    ) {
-        $this->logProductAddToBasketAction((string)$productID);
 
-        return parent::addToBasket($productID, $amount, $sel, $persParam, $shouldOverride, $isBundle, $oldBasketItemId);
-    }
-
-    private function logProductAddToBasketAction(string $productID): void
-    {
-        $basketItemLogger = $this->getService(BasketProductLoggerServiceInterface::class);
-        $basketItemLogger->log($productID);
-    }
 }
